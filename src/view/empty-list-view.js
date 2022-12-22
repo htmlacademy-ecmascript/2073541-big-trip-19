fba7897dec0cca1e4f7e295b0c860be6725e6e6a
Alexandra Bubnova <alexandra.slob@gmail.com>
2022-12-22 20:47:11 +0300

    feat: inherit abstract class in simple components

--- src/view/empty-list-view.js
+++ src/view/empty-list-view.js
@@ -1,27 +1,13 @@
-import { createElement } from '../render.js';
+import AbstractView from '../framework/view/abstract-view.js';
 import { EMPTY_LIST_MESSAGES } from '../const';
 
 function createEmptyListTemplate() {
   return `<p class="trip-events__msg">${EMPTY_LIST_MESSAGES.newEvent}</p>`;
 }
 
-export default class EmptyListView {
-
-  #element = null;
+export default class EmptyListView extends AbstractView {
 
   get template() {
     return createEmptyListTemplate();
-  }
-
-  get element() {
-    if (!this.#element) {
-      this.#element = createElement(this.template);
-    }
-
-    return this.#element;
-  }
-
-  removeElement() {
-    this.#element = null;
   }
 }
