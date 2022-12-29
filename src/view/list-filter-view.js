a271ad4c8490bef3989b3f3926ff0566109281c1
Alexandra Bubnova <alexandra.slob@gmail.com>
2022-12-29 18:35:52 +0300

    refactor: formatting filter view

--- src/view/list-filter-view.js
+++ src/view/list-filter-view.js
@@ -5,12 +5,13 @@ const createFilterItemTemplate = (filters) => {
 
   return (
     `<div class="trip-filters__filter">
-      <input id="filter-${name}"
-      class="trip-filters__filter-input  visually-hidden"
-      type="radio" name="trip-filter"
-      value="${name}"${count === 0 ? 'disabled' : ''}>
-      <label class="trip-filters__filter-label"
-      for="filter-${name}">${name}</label>
+      <input
+        id="filter-${name}"
+        class="trip-filters__filter-input  visually-hidden"
+        type="radio" name="trip-filter"
+        value="${name}"${count === 0 ? 'disabled' : ''}
+      >
+      <label class="trip-filters__filter-label" for="filter-${name}">${name}</label>
     </div>`
   );
 };
