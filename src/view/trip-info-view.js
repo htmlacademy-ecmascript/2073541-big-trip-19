5032e43b4f052d38506b1b66cb4925bba111d81e
Alexandra Bubnova <alexandra.slob@gmail.com>
2023-02-05 22:01:36 +0300

    fix: formatting, methods in class

--- src/view/trip-info-view.js
+++ src/view/trip-info-view.js
@@ -1,81 +1,19 @@
 
 import AbstractView from '../framework/view/abstract-view.js';
 import dayjs from 'dayjs';
+const MAX_VISIBLE_CITIES = 3;
 
-function createTripInfoTemplate(points, offers, destinations) {
-
-  const generateRoute = () => {
-    const cities = points.map((point) => {
-      const city = destinations.find((item) => point.destination === item.id);
-      return city.name;
-    });
-
-    if (cities.length === 0) {
-      return '';
-    }
-    if (cities.length === 1) {
-      return cities[0];
-    }
-    if (cities.length === 2) {
-      return `${cities[0]} &mdash; ${cities[1]}`;
-    }
-    if (cities.length === 3) {
-      return `${cities[0]} &mdash; ${cities[1]} &mdash; ${cities[2]}`;
-    }
-    return `${cities[0]} &mdash; ... &mdash; ${cities[cities.length - 1]}`;
-  };
-
-
-  const generateDates = () => {
-    if (!points.length) {
-      return '';
-    }
-    const firstPointDate = dayjs(points[0].dateFrom);
-    const lastPointDate = dayjs(points[points.length - 1].dateTo);
-    let startDate = firstPointDate.format('D MMM');
-    let endDate = lastPointDate.format('D MMM');
-    if(lastPointDate.isSame(firstPointDate, 'month')) {
-      startDate = firstPointDate.format('MMM D');
-      endDate = lastPointDate.format('DD');
-    }
-
-    return [startDate, endDate].join('  &mdash; ');
-  };
-
-  const generateCost = () => {
-    let totalCost = 0;
-    let offersCost = 0;
-    if (!points.length) {
-      return totalCost;
-    }
-
-    points.forEach((point) => {
-      totalCost += point.basePrice;
-
-      const pointOffers = offers.find((offer) => offer.type === point.type);
-      if (!pointOffers) {
-        return;
-      }
-      const selectedOffers = pointOffers.offers.filter((offer) => point.offers.includes(offer.id));
-      selectedOffers.forEach((offer) => {
-        offersCost += offer.price;
-      });
-    });
-
-    return totalCost + offersCost;
-  };
-
-
+function createTripInfoTemplate(route, dates, cost) {
   return (
     `<section class="trip-main__trip-info  trip-info">
     <div class="trip-info__main">
-      <h1 class="trip-info__title">${generateRoute()}</h1>
+      <h1 class="trip-info__title">${route}</h1>
 
-      <p class="trip-info__dates">${generateDates()}</p>
+      <p class="trip-info__dates">${dates}</p>
     </div>
 
     <p class="trip-info__cost">
-      Total: &euro;&nbsp;<span class="trip-info__cost-value">${generateCost()}</span>
+      Total: &euro;&nbsp;<span class="trip-info__cost-value">${cost}</span>
     </p>
     </section>`
   );
@@ -95,8 +33,69 @@ export default class TripInfoView extends AbstractView {
   }
 
   get template() {
-    return createTripInfoTemplate(this.#points,this.#offers, this.#destinations);
+    const tripRoute = this.getRoute(this.#points, this.#destinations);
+    const tripDates = this.getDates(this.#points);
+    const tripTotalCost = this.getCost(this.#points,this.#offers);
+    return createTripInfoTemplate(tripRoute, tripDates, tripTotalCost);
   }
 
+  getRoute = (points, destinations) => {
+    const cities = points.map((point) => {
+      const city = destinations.find((item) => point.destination === item.id);
 
+      return city.name;
+    });
+
+    if (!cities.length) {
+      return '';
+    }
+
+    if (cities.length > MAX_VISIBLE_CITIES) {
+      return `${cities[0]} &mdash; ... &mdash; ${cities.at(-1)}`;
+    }
+
+    return cities.slice(0, MAX_VISIBLE_CITIES).join(' &mdash; ');
+  };
+
+  getDates = (points) => {
+    if (!points.length) {
+      return '';
+    }
+    const firstPointDate = dayjs(points[0].dateFrom);
+    const lastPointDate = dayjs(points.at(-1).dateTo);
+    let startDate = firstPointDate.format('D MMM');
+    let endDate = lastPointDate.format('D MMM');
+
+    if(lastPointDate.isSame(firstPointDate, 'month')) {
+      startDate = firstPointDate.format('MMM D');
+      endDate = lastPointDate.format('DD');
+    }
+
+    return [startDate, endDate].join('  &mdash; ');
+  };
+
+  getCost = (points, offers) => {
+    let totalCost = 0;
+
+    if (!points.length) {
+      return totalCost;
+    }
+
+    points.forEach((point) => {
+      totalCost += point.basePrice;
+
+      const pointOffers = offers.find((offer) => offer.type === point.type);
+
+      if (!pointOffers) {
+        return;
+      }
+
+      const selectedOffers = pointOffers.offers.filter((offer) => point.offers.includes(offer.id));
+      selectedOffers.forEach((offer) => {
+        totalCost += offer.price;
+      });
+    });
+
+    return totalCost;
+  };
 }
