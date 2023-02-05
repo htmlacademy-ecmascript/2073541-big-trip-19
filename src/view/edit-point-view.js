2a2e0cbdee100c32f58d45cb72cdccd43bb46495
Alexandra Bubnova <alexandra.slob@gmail.com>
2023-02-05 16:49:52 +0300

    feat: add he.encode

--- src/view/edit-point-view.js
+++ src/view/edit-point-view.js
@@ -2,29 +2,29 @@ import dayjs from 'dayjs';
 import AbstractStatefulView from '../framework/view/abstract-stateful-view.js';
 import { capitalizeFirstLetter, isPositiveInteger } from '../utils/utils.js';
 import flatpickr from 'flatpickr';
-//import he from 'he';
+import he from 'he';
 
 import 'flatpickr/dist/flatpickr.min.css';
 
 const getOffersTemplate = (offers, pointOffers) => pointOffers.map((offer) =>
   `<div class="event__offer-selector">
         <input class="event__offer-checkbox  visually-hidden"
-          id="event-offer-${offer.id}" type="checkbox"
-          name=${offer.title} ${offers.includes(offer.id) ? 'checked' : ''} value="${offer.id}">
+          id="event-offer-${he.encode(String(offer.id))}" type="checkbox"
+          name=${he.encode(offer.title)} ${offers.includes(offer.id) ? 'checked' : ''} value="${he.encode(String(offer.id))}">
         <label class="event__offer-label" for="event-offer-${offer.id}">
-          <span class="event__offer-title">${offer.title}</span>
+          <span class="event__offer-title">${he.encode(String(offer.title))}</span>
           &plus;&euro;&nbsp;
-          <span class="event__offer-price">${offer.price}</span>
+          <span class="event__offer-price">${he.encode(String(offer.price))}</span>
         </label>
    </div>`).join('');
 
 const getTypeTemplate = (type, allOffers) => allOffers.map((offer) =>
   `<div class="event__type-item">
-    <input id="event-type-${offer.type}-${offer.id}"
+    <input id="event-type-${he.encode(offer.type)}-${he.encode(String(offer.id))}"
       class="event__type-input  visually-hidden" type="radio"
-      name="event-type" value="${offer.type}" ${type === offer.type ? 'checked' : ''}>
-    <label class="event__type-label  event__type-label--${offer.type}"
-      for="event-type-${offer.type}-${offer.id}">${capitalizeFirstLetter(offer.type)}</label>
+      name="event-type" value="${he.encode(offer.type)}" ${type === offer.type ? 'checked' : ''}>
+    <label class="event__type-label  event__type-label--${he.encode(offer.type)}"
+      for="event-type-${he.encode(offer.type)}-${he.encode(String(offer.id))}">${capitalizeFirstLetter(offer.type)}</label>
   </div>`).join('');
 
 
@@ -34,11 +34,11 @@ const getPointDescription = (destination) => {
     return(
       `<section class="event__section  event__section--destination">
     <h3 class="event__section-title  event__section-title--destination">Destination</h3>
-     <p class="event__destination-description">${destination.description}</p>
+     <p class="event__destination-description">${he.encode(destination.description)}</p>
      <div class="event__photos-container">
         <div class="event__photos-tape">
           ${destination.pictures.map(({ src, description: pictureDescription }) =>
-        `<img class="event__photo" src="${src}" alt="${pictureDescription}">`).join('')}
+        `<img class="event__photo" src="${he.encode(src)}" alt="${he.encode(pictureDescription)}">`).join('')}
         </div>
       </div>
    </section>`);
@@ -47,11 +47,11 @@ const getPointDescription = (destination) => {
 };
 const getDestinationInput = (destination, id) => {
   if(destination) {
-    return `<input class="event__input  event__input--destination" id="event-destination-${id}" type="text" name="event-destination"
-  value=${destination.name} list="destination-list-${id}" >`;
+    return `<input class="event__input  event__input--destination" id="event-destination-${he.encode(String(id))}" type="text" name="event-destination"
+  value=${he.encode(destination.name)} list="destination-list-${he.encode(String(id))}" >`;
   }
-  return `<input class="event__input  event__input--destination" id="event-destination-${id}" type="text" name="event-destination"
- value list="destination-list-${id}" >`;
+  return `<input class="event__input  event__input--destination" id="event-destination-${he.encode(String(id))}" type="text" name="event-destination"
+ value list="destination-list-${he.encode(String(id))}" >`;
 };
 
 function createEditPointTemplate (point, allOffers, destinations, isEditMode ) {
@@ -88,7 +88,7 @@ function createEditPointTemplate (point, allOffers, destinations, isEditMode ) {
           <div class="event__type-wrapper">
             <label class="event__type  event__type-btn" for="event-type-toggle-1">
               <span class="visually-hidden">Choose event type</span>
-              <img class="event__type-icon" width="17" height="17" src="img/icons/${type}.png" alt="Event type icon">
+              <img class="event__type-icon" width="17" height="17" src="img/icons/${he.encode(type)}.png" alt="Event type icon">
             </label>
             <input class="event__type-toggle  visually-hidden" id="event-type-toggle-1" type="checkbox">
 
@@ -105,7 +105,7 @@ function createEditPointTemplate (point, allOffers, destinations, isEditMode ) {
               ${type}
             </label>
             ${getDestinationInput(pointDestination, id)}
-            <datalist id="destination-list-${id}">
+            <datalist id="destination-list-${he.encode(String(id))}">
               ${destinationsList}
             </datalist>
           </div>
@@ -113,11 +113,11 @@ function createEditPointTemplate (point, allOffers, destinations, isEditMode ) {
           <div class="event__field-group  event__field-group--time">
             <label class="visually-hidden" for="event-start-time-1">From</label>
             <input class="event__input  event__input--time" id="event-start-time-1" type="text" name="event-start-time"
-              value='${dayjs(dateFrom).format('DD/MM/YY HH:mm')}'>
+              value='${he.encode(dayjs(dateFrom).format('DD/MM/YY HH:mm'))}'>
             &mdash;
             <label class="visually-hidden" for="event-end-time-1">To</label>
             <input class="event__input  event__input--time" id="event-end-time-1" type="text" name="event-end-time"
-              value='${dayjs(dateTo).format('DD/MM/YY HH:mm')}'>
+              value='${he.encode(dayjs(dateTo).format('DD/MM/YY HH:mm'))}'>
           </div>
 
           <div class="event__field-group  event__field-group--price">
@@ -125,7 +125,7 @@ function createEditPointTemplate (point, allOffers, destinations, isEditMode ) {
               <span class="visually-hidden">Price</span>
               &euro;
             </label>
-            <input class="event__input  event__input--price" id="event-price-1" type="text" name="event-price" value=${basePrice}>
+            <input class="event__input  event__input--price" id="event-price-1" type="text" name="event-price" value=${he.encode(String(basePrice))}>
           </div>
 
           <button class="event__save-btn  btn  btn--blue" type="submit" ${isDisabled ? 'disabled' : ''}>${isSaving ? 'Saving...' : 'Save'}</button>
@@ -226,7 +226,7 @@ export default class EditPointView extends AbstractStatefulView {
 
     if(isPositiveInteger(evt.target.value)) {
       this.updateElement({
-        basePrice: evt.target.value
+        basePrice: +evt.target.value
       });
       return;
     }
